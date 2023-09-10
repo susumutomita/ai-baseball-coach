465bd086f30668e278d685e35e31c3aa9339b1b1
susumu tomita <oyster880@gmail.com>
2023-09-10 09:08:08 +0900

    update pip module

--- setup.py
+++ setup.py
@@ -1,17 +1,15 @@
 from setuptools import setup
 
+with open("requirements.txt", "r") as f:
+    required_packages = f.read().splitlines()
+
 setup(
     name="ai-baseballcoach",
     version="0.1",
     packages=[
         "app",
     ],
-    install_requires=[
-        "pytest-watch",
-        "transformers",
-        "pre-commit",
-        "torch"
-    ],
+    install_requires=required_packages,
     entry_points={
         "console_scripts": [
             "ai-baseballcoach=app.main:main",
