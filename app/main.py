395ec99a1e02a5cc1e458892ba426ad4c64e2108
susumu tomita <oyster880@gmail.com>
2023-09-23 06:19:15 +0900

    update

--- app/main.py
+++ app/main.py
@@ -1,9 +1,5 @@
 import os
 
-from flask import Flask, abort, request
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
 from transformers import pipeline
 
 
@@ -29,7 +25,7 @@ TEAM_RULES = read_all_markdown_files(search_directory)
 prompt_template_path = "./prompt_template.txt"
 PROMPT_FOR_GENERATION_FORMAT = read_markdown_file(prompt_template_path)
 
-pipe = pipeline(task="text-generation", model="EleutherAI/gpt-neo-2.7B")
+pipe = pipeline(task="text-generation", model="meta-llama/Llama-2-7b-chat-hf")
 
 
 def gen_text(prompts, **kwargs):
@@ -40,42 +36,3 @@ def gen_text(prompts, **kwargs):
 
 results = gen_text(["What should a team member do if they are running late?"], max_length=100)
 print(results[0])
-
-app = Flask(__name__)
-
-line_bot_api = LineBotApi("YOUR_CHANNEL_ACCESS_TOKEN")
-handler = WebhookHandler("YOUR_CHANNEL_SECRET")
-
-
-def gen_text(prompts, **kwargs):
-    full_prompts = [f"{PROMPT_FOR_GENERATION_FORMAT}\n{prompt}\n{TEAM_RULES}" for prompt in prompts]
-    outputs = pipe(full_prompts, **kwargs)
-    return [out[0]["generated_text"] for out in outputs]
-
-
-@app.route("/callback", methods=["POST"])
-def callback():
-    signature = request.headers["X-Line-Signature"]
-
-    body = request.get_data(as_text=True)
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-
-    return "OK"
-
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    user_input = event.message.text  # ユーザーからの入力を取得
-    prompts = [f"What should a team member do if {user_input}?"]
-    # ユーザーからの質問に応じた回答を生成
-    responses = gen_text(prompts, max_length=100)
-    response = responses[0]  # 生成されたテキストを取得
-
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=response))
-
-
-if __name__ == "__main__":
-    app.run()
