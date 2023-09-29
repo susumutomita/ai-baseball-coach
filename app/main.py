db2f79d8b27613695e7fd782005b2d4768f4ce9d
susumu tomita <oyster880@gmail.com>
2023-09-29 09:52:53 +0900

    update using pure llama model

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 import os
 
 from llama_cpp import Llama
-from transformers import pipeline
 
 
 def read_all_markdown_files(directory_path):
@@ -25,26 +24,27 @@ TEAM_RULES = read_all_markdown_files(search_directory)
 prompt_template_path = "./prompt_template.txt"
 PROMPT_FOR_GENERATION_FORMAT = read_markdown_file(prompt_template_path)
 
-pipe = pipeline(task="text-generation", model="meta-llama/Llama-2-7b-chat-hf")
-
-
-def gen_text(prompts, **kwargs):
-    full_prompts = [f"{PROMPT_FOR_GENERATION_FORMAT}\n{prompt}\n{TEAM_RULES}" for prompt in prompts]
-    outputs = pipe(full_prompts, **kwargs)
-    return [out[0]["generated_text"] for out in outputs]
-
+# Llama モデルのロード
+model_path = "..//model/llama-2-7b-chat/ggml-model-f16_q4_0.bin"
+model = Llama(
+    model_path=model_path,
+    n_ctx=2048,  # context window size
+    n_gpu_layers=1,  # enable GPU
+    use_mlock=True,
+)  # enable memory lock so not swap
 
 while True:
     user_input = input("質問を入力してください（終了するには'quit'と入力）: ")
     if user_input.lower() == "quit":
         break
-    model_path = "~/llama/llama-2-7b/ggml-model-f32_q4_0.bin"
-    model = Llama(
-        model_path=model_path,
-        n_ctx=2048,  # context window size
-        n_gpu_layers=1,  # enable GPU
-        use_mlock=True,
-    )  # enable memory lock so not swap
-    output = model(prompt=user_input, max_tokens=120, temperature=0.2)
-    output
-    print("応答:", output)
+
+    # プロンプトの整形
+    full_prompt = f"{PROMPT_FOR_GENERATION_FORMAT}\n{user_input}\n{TEAM_RULES}"
+
+    # モデルによるテキスト生成
+    output = model(prompt=full_prompt, max_tokens=120, temperature=0.2)
+
+    # 応答テキストの取得
+    response_text = output.get("choices", [{}])[0].get("text", "No response generated.")
+
+    print("応答:", response_text)
