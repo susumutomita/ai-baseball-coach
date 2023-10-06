f537ed34b38e0e4922fba863a96aa40911a4cf23
susumu tomita <oyster880@gmail.com>
2023-10-07 06:37:36 +0900

    use pipeline

--- app/models/plamo_model.py
+++ app/models/plamo_model.py
@@ -1,21 +1,17 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
+import transformers
 
 from .base_model import BaseModel
 
 
 class PlamoModel(BaseModel):
     def __init__(self, model_name: str):
-        self.tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
-        self.model = AutoModelForCausalLM.from_pretrained(model_name, trust_remote_code=True)
+        self.pipeline = transformers.pipeline(
+            "text-generation", model="pfnet/plamo-13b", trust_remote_code=True
+        )
 
     def load_model(self, model_path: str):
-        # この場合、必要ないかもしれません
         pass
 
     def generate_text(self, prompt: str, max_tokens: int, temperature: float) -> str:
-        inputs = self.tokenizer.encode(prompt, return_tensors="pt")
-        outputs = self.model.generate(
-            inputs, max_length=max_tokens, num_return_sequences=1, temperature=temperature
-        )
-        response_text = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+        response_text = self.pipeline(prompt, max_new_tokens=max_tokens)
         return response_text
