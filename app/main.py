819e1439d691107312468c2441c9a9a3f6e6484a
susumu tomita <oyster880@gmail.com>
2023-10-17 08:08:23 +0900

    use restx

--- app/main.py
+++ app/main.py
@@ -5,40 +5,40 @@ from flask_cors import CORS
 from flask_restx import Api, Resource, fields
 from models import BaseModel, GptModel, PlamoModel
 
+# 定数
+SEARCH_DIRECTORY = "./app/"
+PROMPT_TEMPLATE_PATH = "./prompt_template.txt"
+DEFAULT_MODEL_TYPE = "gpt2"
+
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
 
 
-def read_all_markdown_files(directory_path):
-    team_rules = ""
+def read_files(directory_path, file_extension):
+    content = ""
     for root, dirs, files in os.walk(directory_path):
         for filename in files:
-            if filename.endswith(".ja_jp.md"):
+            if filename.endswith(file_extension):
                 filepath = os.path.join(root, filename)
                 with open(filepath, "r", encoding="utf-8") as file:
-                    team_rules += file.read() + "\n\n"
-    return team_rules
+                    content += file.read() + "\n\n"
+    return content
 
 
-def read_markdown_file(file_path):
-    with open(file_path, "r", encoding="utf-8") as file:
-        return file.read()
+def error_response(message, status_code):
+    return jsonify({"error": message}), status_code
 
 
-model_type = os.environ.get("MODEL_TYPE", "gpt2")
+model_type = os.environ.get("MODEL_TYPE", DEFAULT_MODEL_TYPE)
 
 if model_type == "Plamo":
     model: BaseModel = PlamoModel(model_name="pfnet/plamo-13b")
 elif model_type == "gpt2":
     model: BaseModel = GptModel(model_name="gpt2")
 
-# マークダウンファイルとプロンプトテンプレートの読み込み
-search_directory = "./app/"
-TEAM_RULES = read_all_markdown_files(search_directory)
-prompt_template_path = "./prompt_template.txt"
-PROMPT_FOR_GENERATION_FORMAT = read_markdown_file(prompt_template_path)
-
+TEAM_RULES = read_files(SEARCH_DIRECTORY, ".ja_jp.md")
+PROMPT_FOR_GENERATION_FORMAT = read_files(PROMPT_TEMPLATE_PATH, ".txt")
 
 question_model = api.model(
     "Question",
@@ -49,18 +49,17 @@ question_model = api.model(
 @api.route("/api/question")
 class QuestionResource(Resource):
     @api.expect(question_model, validate=True)
-    @api.doc(
-        params={"question": "The question you want to ask."},
-    )
     def post(self):
-        json_data = request.json
-        if json_data is None:
-            return jsonify({"error": "Invalid input, JSON expected"}), 400
+        if not request.json:
+            return error_response("Invalid input, JSON expected", 400)
 
-        user_input = json_data.get("question")
+        user_input = request.json.get("question")
         if not user_input:
-            return jsonify({"error": "Missing 'question' field in input JSON"}), 400
+            return error_response("Missing 'question' field in input JSON", 400)
 
+        return self.generate_response(user_input)
+
+    def generate_response(self, user_input):
         full_prompt = f"{PROMPT_FOR_GENERATION_FORMAT}\n{user_input}\n{TEAM_RULES}"
         response_text = model.generate_text(prompt=full_prompt, max_tokens=120, temperature=0.2)
 
@@ -69,9 +68,8 @@ class QuestionResource(Resource):
             if "Response:" in response_text
             else response_text
         )
-
         return jsonify({"response": response_only})
 
 
 if __name__ == "__main__":
-    app.run(port=5000, host="0.0.0.0")
+    app.run(host="0.0.0.0")
