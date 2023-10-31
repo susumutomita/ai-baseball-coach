f5028f8f3e5588bc920af09fd26f4e65a85de050
susumu tomita <oyster880@gmail.com>
2023-11-01 06:48:53 +0900

    update auth0

--- tool/infrastructure/main.tf
+++ tool/infrastructure/main.tf
@@ -1,12 +1,14 @@
-resource "auth0_client" "application" {
-  name            = "${var.client_name}-${var.pre_fix}"
-  description     = var.client_description
-  app_type        = var.client_app_type
-  callbacks       = [for domain in var.callback_domains : "${domain}${var.callback_path}"]
-  oidc_conformant = var.oidc_conformant
-
-  jwt_configuration {
-    alg                 = var.jwt_alg
-    lifetime_in_seconds = var.jwt_lifetime_in_seconds
-  }
+# terraform module creating auth0 resources
+module "auth0" {
+  source = "./module/auth0"
+  client_name = var.client_name
+  client_description = var.client_description
+  client_app_type = var.client_app_type
+  client_id = var.client_id
+  client_secret = var.client_secret
+  callback_domains = var.callback_domains
+  callback_path = var.callback_path
+  domain = var.domain
+  jwt_alg = var.jwt_alg
+  jwt_lifetime_in_seconds = var.jwt_lifetime_in_seconds
 }
---
+++ tool/infrastructure/module/auth0/variables.tf
@@ -0,0 +1,71 @@
+variable "client_name" {
+  description = "The name of the Auth0 client"
+  type        = string
+  default     = "ai-baseball-coach"
+}
+
+variable "client_description" {
+  description = "The description of the Auth0 client"
+  type        = string
+  default     = "ai-baseball-coach"
+}
+
+variable "client_app_type" {
+  description = "The type of the Auth0 client app"
+  type        = string
+  default     = "regular_web"
+}
+
+variable "client_id" {
+  description = "The client_id of auth0"
+  type        = string
+  default     = "sample client id"
+}
+
+variable "client_secret" {
+  description = "The client secret of auth0"
+  type        = string
+  default     = "sample client secret"
+}
+
+variable "callback_domains" {
+  description = "List of callback domains"
+  type        = list(string)
+  default     = ["http://127.0.0.1:8080", "http://localhost:8080"]
+}
+
+variable "callback_path" {
+  description = "Callback path"
+  type        = string
+  default     = "/callback"
+}
+
+variable "domain" {
+  description = "The domain of auth0"
+  type        = string
+  default     = "sampledomain.auth0.com"
+}
+
+variable "jwt_alg" {
+  description = "The algorithm used to sign the JWT"
+  type        = string
+  default     = "RS256"
+}
+
+variable "jwt_lifetime_in_seconds" {
+  description = "The lifetime of the JWT in seconds"
+  type        = number
+  default     = 36000
+}
+
+variable "oidc_conformant" {
+  description = "Specify if the client is OIDC Conformant"
+  type        = bool
+  default     = true
+}
+
+variable "pre_fix" {
+  description = "Prefix for the Auth0 client"
+  type        = string
+  default     = "test"
+}
