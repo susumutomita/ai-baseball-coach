657270c1ee12050d84102984097e5d17d270f552
susumu tomita <oyster880@gmail.com>
2023-11-01 06:51:15 +0900

    update auth0

---
+++ tool/infrastructure/module/auth0/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = "= 1.6.2"
+  required_providers {
+    auth0 = {
+      source  = "auth0/auth0"
+      version = ">= 1.0.0"
+    }
+  }
+}
+
+provider "auth0" {
+  domain        = var.domain
+  client_id     = var.client_id
+  client_secret = var.client_secret
+}
