bc5a6d9d59609424f450ea3444f8b8b946bfed70
susumu tomita <oyster880@gmail.com>
2023-11-02 08:34:16 +0900

    add api

--- tool/infrastructure/main.tf
+++ tool/infrastructure/main.tf
@@ -1,14 +1,29 @@
 # terraform module creating auth0 resources
-module "auth0" {
-  source = "./module/auth0"
-  client_name = var.client_name
-  client_description = var.client_description
-  client_app_type = var.client_app_type
-  client_id = var.client_id
-  client_secret = var.client_secret
-  callback_domains = var.callback_domains
-  callback_path = var.callback_path
-  domain = var.domain
-  jwt_alg = var.jwt_alg
+module "auth0-app" {
+  source                  = "./module/auth0"
+  client_name             = var.client_name
+  client_description      = var.client_description
+  client_app_type         = var.client_app_type
+  client_id               = var.client_id
+  client_secret           = var.client_secret
+  callback_domains        = var.callback_domains
+  callback_path           = var.callback_path
+  domain                  = var.domain
+  jwt_alg                 = var.jwt_alg
+  jwt_lifetime_in_seconds = var.jwt_lifetime_in_seconds
+}
+
+module "auth0-api" {
+  source                  = "./module/auth0"
+  client_name             = var.client_name
+  client_description      = var.client_description
+  client_app_type         = "api"
+  client_id               = var.client_id
+  client_secret           = var.client_secret
+  callback_domains        = ""
+  callback_path           = ""
+  pre_fix                 = "api"
+  domain                  = var.domain
+  jwt_alg                 = var.jwt_alg
   jwt_lifetime_in_seconds = var.jwt_lifetime_in_seconds
 }
