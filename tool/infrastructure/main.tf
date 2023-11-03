ca30f53a89430025effe3a4a8655e90e88e1e666
susumu tomita <oyster880@gmail.com>
2023-11-03 09:22:45 +0900

    fix

--- tool/infrastructure/main.tf
+++ tool/infrastructure/main.tf
@@ -16,16 +16,3 @@ module "auth0-api" {
   source = "./module/auth0/api"
   name   = var.client_name
 }
-
-# resource "auth0_client" "application" {
-#   name            = "${var.client_name}-${var.pre_fix}"
-#   description     = var.client_description
-#   app_type        = var.client_app_type
-#   callbacks       = [for domain in var.callback_domains : "${domain}${var.callback_path}"]
-#   oidc_conformant = var.oidc_conformant
-
-#   jwt_configuration {
-#     alg                 = var.jwt_alg
-#     lifetime_in_seconds = var.jwt_lifetime_in_seconds
-#   }
-# }
