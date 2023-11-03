4f5a7ec2bd792b4dfb1f9a6fa6ec115db42802ea
susumu tomita <oyster880@gmail.com>
2023-11-03 09:09:27 +0900

    try

--- tool/infrastructure/main.tf
+++ tool/infrastructure/main.tf
@@ -17,15 +17,15 @@
 #   name   = var.client_name
 # }
 
-resource "auth0_client" "application" {
-  name            = "${var.client_name}-${var.pre_fix}"
-  description     = var.client_description
-  app_type        = var.client_app_type
-  callbacks       = [for domain in var.callback_domains : "${domain}${var.callback_path}"]
-  oidc_conformant = var.oidc_conformant
+# resource "auth0_client" "application" {
+#   name            = "${var.client_name}-${var.pre_fix}"
+#   description     = var.client_description
+#   app_type        = var.client_app_type
+#   callbacks       = [for domain in var.callback_domains : "${domain}${var.callback_path}"]
+#   oidc_conformant = var.oidc_conformant
 
-  jwt_configuration {
-    alg                 = var.jwt_alg
-    lifetime_in_seconds = var.jwt_lifetime_in_seconds
-  }
-}
+#   jwt_configuration {
+#     alg                 = var.jwt_alg
+#     lifetime_in_seconds = var.jwt_lifetime_in_seconds
+#   }
+# }
