451f95c7931ac6027a0859ad0148fd1a7d727d58
susumu tomita <oyster880@gmail.com>
2023-11-11 05:37:17 +0900

    remove auth0

--- app/auth/auth.py
+++ app/auth/auth.py
@@ -9,8 +9,8 @@ def setup_auth(app):
         client_id=app.config["AUTH0_CLIENT_ID"],
         client_secret=app.config["AUTH0_CLIENT_SECRET"],
         client_kwargs={"scope": "openid profile email"},
-        server_metadata_url=f'https://{app.config["AUTH0_DOMAIN"]}/'
-        ".well-known/openid-configuration",
+        server_metadata_url=(
+            f'https://{app.config["AUTH0_DOMAIN"]}/' ".well-known/openid-configuration"
+        ),
     )
-
     return oauth
