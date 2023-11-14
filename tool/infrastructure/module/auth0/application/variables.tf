d81c6ce47c370a6b241c26fcc1d36b84f4e85121
susumu tomita <oyster880@gmail.com>
2023-11-14 09:19:18 +0900

    add logouturl

--- tool/infrastructure/module/auth0/application/variables.tf
+++ tool/infrastructure/module/auth0/application/variables.tf
@@ -1,21 +1,3 @@
-variable "client_name" {
-  description = "The name of the Auth0 client"
-  type        = string
-  default     = "ai-baseball-coach"
-}
-
-variable "client_description" {
-  description = "The description of the Auth0 client"
-  type        = string
-  default     = "ai-baseball-coach"
-}
-
-variable "client_app_type" {
-  description = "The type of the Auth0 client app"
-  type        = string
-  default     = "regular_web"
-}
-
 variable "callback_domains" {
   description = "List of callback domains"
   type        = list(string)
@@ -28,6 +10,24 @@ variable "callback_path" {
   default     = "/callback"
 }
 
+variable "client_app_type" {
+  description = "The type of the Auth0 client app"
+  type        = string
+  default     = "regular_web"
+}
+
+variable "client_description" {
+  description = "The description of the Auth0 client"
+  type        = string
+  default     = "AI Baseball Coach"
+}
+
+variable "client_name" {
+  description = "The name of the Auth0 client"
+  type        = string
+  default     = "ai-baseball-coach"
+}
+
 variable "jwt_alg" {
   description = "The algorithm used to sign the JWT"
   type        = string
@@ -38,6 +38,12 @@ variable "jwt_lifetime_in_seconds" {
   description = "The lifetime of the JWT in seconds"
   type        = number
   default     = 36000
+}
+
+variable "logout_urls" {
+  description = "List of allowed logout URLs"
+  type        = list(string)
+  default     = ["http://127.0.0.1:8080/home", "http://localhost:8080/home", "http://127.0.0.1:5000/home", "http://localhost:5000/home"]
 }
 
 variable "oidc_conformant" {
