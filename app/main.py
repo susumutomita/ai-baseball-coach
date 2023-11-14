b7e3b30b32f6e3f98acc94d9feb4efdd6e5fc6df
susumu tomita <oyster880@gmail.com>
2023-11-14 09:22:47 +0900

    disable debug mode

--- app/main.py
+++ app/main.py
@@ -121,4 +121,4 @@ class QuestionResource(Resource):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=app.config["PORT"], debug=True)
+    app.run(host="0.0.0.0", port=app.config["PORT"])
