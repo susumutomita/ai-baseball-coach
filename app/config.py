e63f6fc41925e63a5be7cc524acc1b1ef979bb41
susumu tomita <oyster880@gmail.com>
2023-11-19 07:47:02 +0900

    add config

--- app/config.py
+++ app/config.py
@@ -8,6 +8,7 @@ class Config(object):
     AUTH0_CLIENT_ID = os.environ.get("AUTH0_CLIENT_ID")
     AUTH0_CLIENT_SECRET = os.environ.get("AUTH0_CLIENT_SECRET")
     AUTH0_DOMAIN = os.environ.get("AUTH0_DOMAIN")
+    API_AUDIENCE = os.getenv("API_AUDIENCE")
     DEFAULT_MODEL_TYPE = "gpt2"
     MODEL_TYPE = os.environ.get("MODEL_TYPE", DEFAULT_MODEL_TYPE)
     PORT = os.environ.get("PORT", 8080)
